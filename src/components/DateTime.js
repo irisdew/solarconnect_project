23738e095014fd8d97d3980423591731fddfdd05
hami-dev <rinrin.xml@gmail.com>
2021-08-17 22:15:06 +0900

    refactor : style 및 DateTime 코드 수정

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function DateTime({ type }) {
-  const [time, setTime] = useState(null);
   const date = new Date();
   const options = {
     weekday: 'long',
@@ -10,19 +9,11 @@ function DateTime({ type }) {
     day: 'numeric',
   };
 
-  useEffect(() => {
-    setTime(date.toLocaleDateString(type, options));
-  }, []);
-
-  setInterval(() => {
-    setTime(date.toLocaleDateString(type, options));
-  }, 18000);
-
-  console.log(time);
   return (
     <div className="wrapper__time">
-      <p>{time}</p>
+      {date.toLocaleDateString(type, options)}
     </div>
   );
 }
+
 export default DateTime;
