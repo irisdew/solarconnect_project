ea5cc5b8d8c49063c427c185327f5a27d3a12941
hami-dev <rinrin.xml@gmail.com>
2021-08-17 15:02:43 +0900

    feature : 현재 시간을 출력하는 기능 구현

---
+++ src/Constant.js
@@ -0,0 +1,24 @@
+export const korWeek = ["일", "월", "화", "수", "목", "금", "토"];
+export const usWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+export const usMonth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
