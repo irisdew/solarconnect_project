b50cf95ba3862958ee9db25f513cb0cc3d3aaaeb
Seokkitdo <sjd12329@gmail.com>
2021-08-17 21:32:10 +0900

    refactor : 폰트 및 크기 & 실시간

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,19 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
 function DateTime({ type }) {
-    const date = new Date();
-    const options = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-    };
+  const [time, setTime] = useState(null)
+  const date = new Date()
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
 
-    return (
-        <div className="wrapper__time">
-            <p>{date.toLocaleDateString(type, options)}</p>
-        </div>
-    );
+  useEffect(() => {
+    setTime(date.toLocaleDateString(type, options))
+  }, [])
+
+  setInterval(() => {
+    setTime(date.toLocaleDateString(type, options))
+  }, 18000)
+
+  console.log(time)
+  return (
+    <div className="wrapper__time">
+      <p>{time}</p>
+    </div>
+  )
 }
-
-export default DateTime;
+// 혹시 rafce 안쓰시는지 - 혜린님
+export default DateTime
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,47 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
-import {sort, makeArrayfromString} from 'utils/sort'
+import { sort, makeArrayfromString } from 'utils/sort'
 function Result({ inputValue, desc }) {
-    const [ascSort, setAscSort] = useState([]);
-    const [descSort, setDesccSort] = useState([]);
-    
-    const [waiting, setWaiting] = useState(true);
+  const [ascSort, setAscSort] = useState([])
+  const [descSort, setDesccSort] = useState([])
 
-    useEffect(() => {
-        init();
-        setWaiting(true);
-    }, [inputValue]);
+  const [waiting, setWaiting] = useState(true)
 
+  useEffect(() => {
+    inputValue && init()
+    setWaiting(true)
+  }, [inputValue])
 
-    function init() {
-        const arr = makeArrayfromString(inputValue);
-        setAscSort(sort(arr, false))
-        setTimeout(() => {
-            setDesccSort(sort(arr, true))
-            setWaiting(false);
-        }, 3000);
-    }
+  function init() {
+    const arr = makeArrayfromString(inputValue)
+    setAscSort(sort(arr, false))
+    setTimeout(() => {
+      setDesccSort(sort(arr, true))
+      setWaiting(false)
+    }, 3000)
+  }
 
-    return (
-        <div className="result">
-            <div className="result__title">
-                {desc ? "내림차순" : "오름차순"}
-            </div>
-            <div className="result__content">
-                {desc ? (
-                    <>
-                        {waiting ? (
-                            "waiting"
-                        ) : (
-                            <span>{descSort.join(", ")}</span>
-                        )}
-                    </>
-                ) : (
-                    <span>{ascSort.join(", ")}</span>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="result">
+      <div className="result__title">{desc ? '내림차순' : '오름차순'}</div>
+      <div className="result__content">
+        {desc ? (
+          <>{waiting ? 'waiting' : <span>{descSort.join(', ')}</span>}</>
+        ) : (
+          <span>{ascSort.join(', ')}</span>
+        )}
+      </div>
+    </div>
+  )
 }
 
-export default Result;
+export default Result
